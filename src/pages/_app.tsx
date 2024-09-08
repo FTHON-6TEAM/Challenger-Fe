26aa5fe522498cd49dbaa5e1d52cfdd882151fd9
breeiee <breeieiee@gmail.com>
2024-09-08 23:58:55 +0900

    feat: Add Material-UI, theme for styling

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,10 @@
 import type { AppProps } from 'next/app';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import React from 'react';
-import ResetCSS from '@/styles/ResetCSS';
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material/styles';
+import { theme } from '../styles/theme';
+import ResetCSS from '../styles/ResetCSS';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -17,11 +20,12 @@ const queryClient = new QueryClient({
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
       <ResetCSS />
       <QueryClientProvider client={queryClient}>
         <Component {...pageProps} />
       </QueryClientProvider>
-    </>
+    </ThemeProvider>
   );
 }
