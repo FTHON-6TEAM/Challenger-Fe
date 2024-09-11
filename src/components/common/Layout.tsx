d53c28a4f3ef3a360847294cbee70348b3132ad7
breeiee <breeieiee@gmail.com>
2024-09-12 03:48:25 +0900

    feat: Implement default layout

---
+++ src/components/common/Layout.tsx
@@ -0,0 +1,19 @@
+import React, { PropsWithChildren } from 'react';
+import Sidebar from './Sidebar';
+import Header from './Header';
+import { Container, MainContainer } from './styles/layout';
+import useBoolean from '../../hooks/common/useBoolean'; // 여기만 경로 못찾음..
+
+const Layout = ({ children }: PropsWithChildren) => {
+  const [isOpenDrawer, openDrawer, closeDrawer] = useBoolean();
+
+  return (
+    <Container>
+      <Header openDrawer={openDrawer} />
+      <Sidebar isOpen={isOpenDrawer} closeDrawer={closeDrawer} />
+      <MainContainer maxWidth={false}>{children}</MainContainer>
+    </Container>
+  );
+};
+
+export default Layout;
