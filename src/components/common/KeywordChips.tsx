66a43a330e2194bd6abf979a4d919d50299772a8
breeiee <breeieiee@gmail.com>
2024-09-13 19:28:12 +0900

    feat: Implement chip list

---
+++ src/components/common/KeywordChips.tsx
@@ -0,0 +1,32 @@
+import useGetCodeList from '@/hooks/code/useGetCodeList';
+import { Box, Chip, Skeleton } from '@mui/material';
+
+//TODO : color 추가
+
+type KeywordChipsProps = {
+  disabled?: boolean;
+  onClickChip: (code: string) => void;
+};
+
+const KeywordChips = (props: KeywordChipsProps) => {
+  const { disabled = false, onClickChip } = props;
+  const { isLoading, data } = useGetCodeList();
+
+  return (
+    <Box display={'flex'} alignItems={'center'} gap={1}>
+      {isLoading && <Skeleton />}
+      {!isLoading &&
+        data &&
+        data.map((item) => (
+          <Chip
+            key={item.code}
+            label={item.title}
+            disabled={disabled}
+            onClick={() => onClickChip(item.code)}
+          />
+        ))}
+    </Box>
+  );
+};
+
+export default KeywordChips;
